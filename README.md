6dea7221598d93792d6d54ecae9c934459d29cea
adamdelsol <adamdelsol@grace.myfiosgateway.com>
2022-12-22 19:36:20 -0500

    airdrop to wallet

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # spl-creator
-# spl-creator
+
+- install [solana](https://docs.solana.com/cli/install-solana-cli-tools)
+- create wallet
+- run spl-creator
--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-import { Metaplex } from "@metaplex-foundation/js";
-import { Connection, clusterApiUrl } from "@solana/web3.js";
+import { Connection, clusterApiUrl, Keypair, LAMPORTS_PER_SOL } from "@solana/web3.js";
 
 // provides our connection to the Solana devnet network
 const connection = new Connection(clusterApiUrl("devnet"));
-// uses the devnet connection to interact with the Metaplex programs
-const metaplex = new Metaplex(connection);
+
+const authorityWallet = Keypair.generate();
+console.log(`Wallet: ${authorityWallet.publicKey}`);
+
+const authorityAirdropSignature = await connection.requestAirdrop(
+    authorityWallet.publicKey,
+    LAMPORTS_PER_SOL
+)
+
+await connection.confirmTransaction(authorityAirdropSignature, {
+    commitment: "confirmed",
+})
+
+let authorityBalance = await connection.getBalance(authorityWallet.publicKey);
+
+console.log(`Authority Wallet Balance: ${authorityBalance}`);
